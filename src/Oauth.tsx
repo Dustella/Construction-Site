f94eab31228deb6561a81e0137d4b21d07390f53
Dustella Chan <fdnoaivj@outlook.com>
2022-08-01 14:34:34 +0800

    ✨ go here and jump

--- src/Oauth.tsx
+++ src/Oauth.tsx
@@ -1,8 +1,9 @@
 import { Component } from "solid-js";
 import { useSearchParams } from "@solidjs/router";
 export const OAuth:Component = () => {
-    const [{code},] = useSearchParams()
-    console.log(code)
+    const [{code,state},] = useSearchParams()
+    console.log(code,state)
+    location.href = `https://zq.game.maiquer.tech/oauth?code=${code}&state=${state}`
     return (
         <div>正在跳转</div>
     )
